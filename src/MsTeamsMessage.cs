89ced2059f9f1a1533a15fc7ec664d54980e9898
Jake Soenneker <jake@soenneker.com>
2025-03-22 19:58:13 -0500

    Initial commit

---
+++ src/MsTeamsMessage.cs
@@ -0,0 +1,45 @@
+﻿using AdaptiveCards;
+using Newtonsoft.Json;
+using Soenneker.Dtos.AdaptiveCard.Attachments;
+using Soenneker.Dtos.MsTeams.Card;
+using Soenneker.Messages.Base;
+using System.Text.Json.Serialization;
+
+namespace Soenneker.Messages.MsTeams;
+
+/// <summary>
+/// A Service Bus Message for Microsoft Teams webhook sending
+/// </summary>
+public class MsTeamsMessage : Message
+{
+    [JsonProperty("msTeamsCard")]
+    [JsonPropertyName("msTeamsCard")]
+    public MsTeamsCard MsTeamsCard { get; set; } = null!;
+
+    [JsonProperty("channel")]
+    [JsonPropertyName("channel")]
+    public string Channel { get; set; } = null!;
+
+    public MsTeamsMessage() : base("msteams")
+    {
+        NewtonsoftSerialize = true;
+    }
+
+    public MsTeamsMessage(AdaptiveCard adaptiveCard, string channel) : base("msteams")
+    {
+        MsTeamsCard = new MsTeamsCard
+        {
+            Type = "message",
+            Attachments =
+            [
+                new AdaptiveCardAttachments
+                {
+                    Content = adaptiveCard
+                }
+            ]
+        };
+
+        NewtonsoftSerialize = true;
+        Channel = channel;
+    }
+}
---
+++ test/Soenneker.Messages.MsTeams.Tests/MsTeamsMessageTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Messages.MsTeams.Tests;
+
+[Collection("Collection")]
+public class MsTeamsMessageTests : FixturedUnitTest
+{
+    public MsTeamsMessageTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
