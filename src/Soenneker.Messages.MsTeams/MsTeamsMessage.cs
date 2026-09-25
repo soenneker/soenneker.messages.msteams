fc3546ddc7b4675c5d05511d42d69013d8efcf02
soenneker <jake@soenneker.com>
2026-09-24 21:19:25 -0500

    deps

--- src/Soenneker.Messages.MsTeams/MsTeamsMessage.cs
+++ src/Soenneker.Messages.MsTeams/MsTeamsMessage.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using Soenneker.Dtos.MsTeams.Card;
+﻿using Soenneker.Dtos.MsTeams.Card;
 using Soenneker.Messages.Base;
 using System.Text.Json.Serialization;
 
@@ -15,13 +14,11 @@ public sealed class MsTeamsMessage : Message
     /// object initialization.
     /// </summary>
     [JsonPropertyName("msTeamsCard")]
-    [JsonProperty("msTeamsCard", Required = Required.Always)]
     public required MsTeamsCard MsTeamsCard { get; set; }
 
     /// <summary>
     /// Gets the name of the channel associated with this instance.
     /// </summary>
     [JsonPropertyName("channel")]
-    [JsonProperty("channel", Required = Required.Always)]
     public required string Channel { get; set; }
 }
